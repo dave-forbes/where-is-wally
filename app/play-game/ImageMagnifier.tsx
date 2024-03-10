061e17c084fb5fc63c353ec04279f36d685ad77a
David Forbes <davidforbesi@hotmail.com>
2024-03-10 22:57:53 +0000

    add new magnifying glass component
    - code taken from this article https://dev.to/anxiny/create-an-image-magnifier-with-react-3fd7

---
+++ app/play-game/ImageMagnifier.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+
+//- code taken from this article https://dev.to/anxiny/create-an-image-magnifier-with-react-3fd7
+
+export default function ImageMagnifier({
+  src,
+  width,
+  height,
+  magnifierHeight = 120,
+  magnifierWidth = 120,
+  zoomLevel = 1.2,
+}: {
+  src: string;
+  width?: string;
+  height?: string;
+  magnifierHeight?: number;
+  magnifierWidth?: number;
+  zoomLevel?: number;
+}) {
+  const [[x, y], setXY] = useState([0, 0]);
+  const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+  const [showMagnifier, setShowMagnifier] = useState(false);
+  return (
+    <div
+      style={{
+        position: "relative",
+        height: height,
+        width: width,
+        cursor: "none",
+      }}
+    >
+      <img
+        src={src}
+        style={{ height: height, width: width }}
+        onMouseEnter={(e) => {
+          // update image size and turn-on magnifier
+          const elem = e.currentTarget;
+          const { width, height } = elem.getBoundingClientRect();
+          setSize([width, height]);
+          setShowMagnifier(true);
+        }}
+        onMouseMove={(e) => {
+          // update cursor position
+          const elem = e.currentTarget;
+          const { top, left } = elem.getBoundingClientRect();
+
+          // calculate cursor position on the image
+          const x = e.pageX - left - window.pageXOffset;
+          const y = e.pageY - top - window.pageYOffset;
+          setXY([x, y]);
+        }}
+        onMouseLeave={() => {
+          // close magnifier
+          setShowMagnifier(false);
+        }}
+        alt={"img"}
+      />
+
+      <div
+        style={{
+          display: showMagnifier ? "" : "none",
+          position: "absolute",
+
+          // prevent magnifier blocks the mousemove event of img
+          pointerEvents: "none",
+          // set size of magnifier
+          height: `${magnifierHeight}px`,
+          width: `${magnifierWidth}px`,
+          // move element center to cursor pos
+          top: `${y - magnifierHeight / 2}px`,
+          left: `${x - magnifierWidth / 2}px`,
+          opacity: "1", // reduce opacity so you can verify position
+          border: "4px solid grey",
+          outline: "2px solid black",
+          borderRadius: "50%",
+          backgroundImage: `url(${src})`,
+          backgroundRepeat: "no-repeat",
+
+          //calculate zoomed image size
+          backgroundSize: `${imgWidth * zoomLevel}px ${
+            imgHeight * zoomLevel
+          }px`,
+
+          //calculate position of zoomed image.
+          backgroundPositionX: `${-x * zoomLevel + magnifierWidth / 2}px`,
+          backgroundPositionY: `${-y * zoomLevel + magnifierHeight / 2}px`,
+        }}
+      ></div>
+    </div>
+  );
+}
--- app/play-game/page.tsx
+++ app/play-game/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import ImageSelector from "./ImageSelector";
+import ImageMagnifier from "./ImageMagnifier";
 
 export default function Page() {
+  const handleClick = (e: any) => {
+    console.log({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-    <div className="flex items-center h-screen w-screen justify-center">
-        <ImageSelector />
+    <div
+      className="flex items-center h-screen w-screen justify-center bg-black"
+      onClick={handleClick}
+    >
+      <ImageMagnifier
+        src={"/48-Where's-Wally-WALKER-BOOKS.jpg"}
+        width={"900"}
+        height={"660"}
+      />
     </div>
   );
 }
