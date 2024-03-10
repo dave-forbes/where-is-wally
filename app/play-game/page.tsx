2f91f66802e27b61301bf564dea230559a3112ea
David Forbes <davidforbesi@hotmail.com>
2024-03-10 17:35:18 +0000

    create game page

---
+++ app/play-game/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ImageSelector from "./ImageSelector";
+
+export default function Page() {
+  return (
+    <div className="flex items-center h-screen w-screen justify-center">
+        <ImageSelector />
+    </div>
+  );
+}
