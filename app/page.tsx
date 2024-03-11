a4b570a6c77889e47a1e7e541291fd12aef3ea6c
David Forbes <davidforbesi@hotmail.com>
2024-03-11 15:20:38 +0000

    modify text content

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center flex-col gap-4 bg-black text-white">
-      <h1 className="text-7xl">Where's wally!</h1>
-      <h3>(In the UK we call it wally not waldo...)</h3>
-      <div className="flex gap-5">
-        <Link href="/play-game/easy">
-          <button className="hover:bg-green-700 bg-green-500 rounded-lg p-3">
-            Easy
-          </button>
-        </Link>
-        <Link href="/play-game/medium">
-          <button className="hover:bg-orange-500 bg-orange-300 rounded-lg p-3">
-            Medium
-          </button>
-        </Link>
-        <Link href="/play-game/hard">
-          <button className="hover:bg-red-700 bg-red-500 rounded-lg p-3">
-            Hard
-          </button>
-        </Link>
+    <div className="h-screen flex justify-center items-center  bg-black text-white ">
+      <div className="max-w-[600px] flex justify-center items-center flex-col gap-4">
+        <h1 className="text-7xl">Where's wally!</h1>
+        <h3>(In the UK we call him wally not waldo...)</h3>
+        <p>
+          Wally and his associates are are wanted by every intellegence agency
+          in the world for numourous charges. You have been assigned to take
+          them out.
+        </p>
+        <p>Select your mission</p>
+        <div className="flex gap-5">
+          <Link href="/play-game/easy">
+            <button className="hover:bg-green-700 bg-green-500 rounded-lg p-3">
+              Easy
+            </button>
+          </Link>
+          <Link href="/play-game/medium">
+            <button className="hover:bg-orange-500 bg-orange-300 rounded-lg p-3">
+              Medium
+            </button>
+          </Link>
+          <Link href="/play-game/hard">
+            <button className="hover:bg-red-700 bg-red-500 rounded-lg p-3">
+              Hard
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
