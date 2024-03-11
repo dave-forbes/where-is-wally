3e12a9df9144df71f808e44c0e5663b173e21284
David Forbes <davidforbesi@hotmail.com>
2024-03-11 11:58:26 +0000

    add icon and adjust styles
    - added padding and confirm target button for sending fetch request

--- app/play-game/SelectPopup.tsx
+++ app/play-game/SelectPopup.tsx
@@ -1,5 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import React from "react";
+import Image from "next/image";
+import wallyIcon from "../../public/wallyico.png";
 
 interface SelectPopupProps {
   opacity: number;
@@ -12,13 +14,16 @@ const SelectPopup = ({ opacity, position }: SelectPopupProps) => {
       {opacity > 0 && (
         <motion.div
           initial={{ opacity: 0, height: 0 }}
-          animate={{ opacity: opacity, height: "100px" }}
+          animate={{ opacity: opacity, height: "auto" }}
           exit={{ opacity: 0, height: 0 }}
           transition={{ duration: 0.3 }}
-          className="absolute bg-white w-[100px] rounded-lg overflow-hidden"
+          className="absolute bg-white p-3 rounded-lg overflow-hidden flex flex-col justify-center items-center gap-2"
           style={{ top: `${position.y}px`, left: `${position.x}px` }}
         >
-          <h1>hi</h1>
+          <Image src={wallyIcon} alt={""} className="w-16 h-16" />
+          <button className="bg-black text-white rounded-lg p-2">
+            Confirm target?
+          </button>
         </motion.div>
       )}
     </AnimatePresence>
