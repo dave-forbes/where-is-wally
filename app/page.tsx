9335370523e33ab8cebfc4dea4a05dd71a315154
David Forbes <davidforbesi@hotmail.com>
2024-03-11 13:42:15 +0000

    modify links
    - changed links to correct dynamic path
    - change bg color to black and text to white

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen flex justify-center items-center flex-col gap-4">
+    <div className="h-screen flex justify-center items-center flex-col gap-4 bg-black text-white">
       <h1 className="text-7xl">Where's wally!</h1>
       <h3>(In the UK we call it wally not waldo...)</h3>
-      <Link href="/play-game">
-        <button className="text-blue-500 hover:text-blue-700">play game</button>
-      </Link>
+      <div className="flex gap-5">
+        <Link href="/play-game/easy">
+          <button className="hover:bg-green-700 bg-green-500 rounded-lg p-3">
+            Easy
+          </button>
+        </Link>
+        <Link href="/play-game/medium">
+          <button className="hover:bg-orange-500 bg-orange-300 rounded-lg p-3">
+            Medium
+          </button>
+        </Link>
+        <Link href="/play-game/hard">
+          <button className="hover:bg-red-700 bg-red-500 rounded-lg p-3">
+            Hard
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
