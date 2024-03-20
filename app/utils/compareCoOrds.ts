4637eb633fa80bf4a2a3d18799e7d55effdf4c59
David Forbes <davidforbesi@hotmail.com>
2024-03-20 11:42:26 +0000

    update compareCoOrds
    - adjust variance so that it lines up with headshot and nothing else
    - make sure withinrange compares with values to 2dp

--- app/utils/compareCoOrds.ts
+++ app/utils/compareCoOrds.ts
@@ -4,15 +4,17 @@ interface Coords {
 }
 
 export default function compareCoords(coord1: Coords, coord2: Coords): boolean {
-  const varianceAmountX = 0.01;
-  const varianceAmountY = 0.03;
+  const varianceAmountX = 0.005;
+  const varianceAmountY = 0.01;
 
   const withinRange = (
     val1: number,
     val2: number,
     varianceAmount: number
   ): boolean => {
-    return Math.abs(val1 - val2) <= varianceAmount;
+    const result = Math.abs(val1 - val2);
+    const result2DP = parseFloat(result.toFixed(2));
+    return result2DP <= varianceAmount;
   };
 
   return (
