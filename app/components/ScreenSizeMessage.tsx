dfc5cba3733b02950a94d281c2fdee4d3641e420
David Forbes <davidforbesi@hotmail.com>
2024-03-22 14:20:46 +0000

    create screensizemessage component
    - for displaying if screen size not big enough

---
+++ app/components/ScreenSizeMessage.tsx
@@ -0,0 +1,7 @@
+export default function ScreenSizeMessage() {
+  return (
+    <h1 className="text-5xl text-center text-white">
+      Sorry, this app is only designed to run on desktop at the moment!
+    </h1>
+  );
+}
