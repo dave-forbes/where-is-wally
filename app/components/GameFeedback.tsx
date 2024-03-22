98ebe6e6a689e15ca50b375ed9cb424a905466f7
David Forbes <davidforbesi@hotmail.com>
2024-03-22 11:57:13 +0000

    use gameFeedback state for content

--- app/components/GameFeedback.tsx
+++ app/components/GameFeedback.tsx
@@ -1,3 +1,8 @@
+import { useGameContext } from "../Context/GameContext";
+
 export default function GameFeedback() {
-  return <></>;
+  const { gameFeedback } = useGameContext();
+  return (
+    <h1 className="text-4xl text-white absolute bottom-10">{gameFeedback}</h1>
+  );
 }
