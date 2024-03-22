f3a6327656e1937f09b58bf6c31cbc6b7cc2904a
David Forbes <davidforbesi@hotmail.com>
2024-03-22 12:01:59 +0000

    update win modal
    - move gameOver rending logic to parent
    - changed score post request to send total seconds instead of formated time, so easier to rank when displaying scoreboard
    - add error handling feedback in case fail to submit
    - added layout styles and other styling to modal make ui look clearer

--- app/components/WinModal.tsx
+++ app/components/WinModal.tsx
@@ -5,91 +5,90 @@ import { collection, addDoc } from "firebase/firestore";
 import db from "../utils/firebase";
 
 const WinModal = () => {
-  const {
-    gameOver,
-    setGameOver,
-    setFoundCharacters,
-    difficulty,
-    totalSeconds,
-  } = useGameContext();
+  const { setFoundCharacters, difficulty, totalSeconds } = useGameContext();
   const [playerName, setPlayerName] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    // Calculate minutes and seconds
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-
-    // Format minutes and seconds to two decimal places
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    const formattedSeconds = String(seconds).padStart(2, "0");
-
     const data = {
       name: playerName,
       difficulty: difficulty,
-      time: `${formattedMinutes}m ${formattedSeconds}s`,
+      time: totalSeconds,
     };
     try {
-      const docRef = await addDoc(collection(db, "scoreboard"), data);
-      console.log("Document added with ID: ", docRef.id);
-    } catch (error) {
-      console.log(error);
+      await addDoc(collection(db, "scoreboard"), data);
+    } catch (error: any) {
+      setError("Error: Failed to submit score, please try again later");
     }
     setFoundCharacters([]);
-    setGameOver(false);
     setSubmitted(true);
   };
 
   if (submitted) {
     return (
       <div className="modal-overlay">
-        <div className="modal-content">
-          <h1>Score submited</h1>
-          {difficulty !== "hard" && (
-            <Link
-              href={
-                difficulty === "easy" ? "/play-game/medium" : "/play-game/hard"
-              }
-            >
-              <button className="hover:bg-green-700 bg-green-500 rounded-lg p-3">
-                Next Level
-              </button>
-            </Link>
+        <div className="modal-content flex flex-col gap-6 items-center">
+          {error ? (
+            <h1 className="text-2xl">{error}</h1>
+          ) : (
+            <>
+              <h1 className="text-2xl">Score submited</h1>
+              {difficulty !== "hard" && (
+                <Link
+                  href={
+                    difficulty === "easy"
+                      ? "/play-game/medium"
+                      : "/play-game/hard"
+                  }
+                >
+                  <button className="hover:bg-green-700 bg-green-500 rounded-lg p-3">
+                    Next Level
+                  </button>
+                </Link>
+              )}
+              <Link href="/scoreboard">
+                <button className="hover:bg-blue-700 bg-blue-500 rounded-lg p-3">
+                  View Scoreboard
+                </button>
+              </Link>
+            </>
           )}
-          <Link href="/scoreboard">
-            <button className="hover:bg-green-700 bg-blue-500 rounded-lg p-3">
-              View Scoreboard
-            </button>
-          </Link>
         </div>
       </div>
     );
-  }
-
-  if (gameOver && !submitted) {
+  } else {
     return (
       <div className="modal-overlay">
-        <div className="modal-content">
-          <h2>Congratulations! You won!</h2>
-          <form onSubmit={handleSubmit} className=" flex flex-col">
-            <label>
-              Enter your name:
+        <div className="modal-content  flex flex-col gap-6 items-center">
+          <h2 className="text-2xl">Mission completed!</h2>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-6 items-center"
+          >
+            <label className="flex flex-col gap-3 items-center">
+              Enter your name
               <input
+                className="border-2 border-black rounded-lg p-1"
                 type="text"
                 value={playerName}
                 onChange={(e) => setPlayerName(e.target.value)}
                 required
               />
             </label>
-            <button type="submit">Submit Score</button>
+            <button
+              className="hover:bg-green-700 bg-green-500 rounded-lg p-3"
+              type="submit"
+            >
+              Submit Score
+            </button>
           </form>
         </div>
       </div>
     );
   }
-  return <></>;
 };
 
 export default WinModal;
